a5fb33b0bcc86340f0a3db059b4c7d9f8d8510a6
sai <sai@DESKTOP-3S42E7Q>
2018-05-09 14:44:15 +0530

    gitjen demo build1

---
+++ GitJens/src/gitGen/GitJen.java
@@ -0,0 +1,14 @@
+package gitGen;
+
+import org.testng.annotations.Test;
+
+public class GitJen {
+	@Test
+	public void display()
+	{
+		for(int i=0;i<10;i++) {
+		System.out.println("My Test SCrupt");}
+		
+	}
+
+}
